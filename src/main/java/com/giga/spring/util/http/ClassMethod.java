18a32de00850b89bc07bcee1d43e5ccf7f55389c
Yvan Noah <yvannandy@gmail.com>
2025-11-21 16:50:57 +0300

    Implement parameterValue = request.getParameter(parameterName)

--- src/main/java/com/giga/spring/util/http/ClassMethod.java
+++ src/main/java/com/giga/spring/util/http/ClassMethod.java
@@ -3,6 +3,9 @@ package com.giga.spring.util.http;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 public class ClassMethod {
     private Class <?> c;
@@ -18,6 +21,25 @@ public class ClassMethod {
         Constructor<?> controllerConstructor = c.getDeclaredConstructor();
         Object controller = controllerConstructor.newInstance();
         return m.invoke(controller);
+    }
+
+    public Object invokeMethod(HttpServletRequest req) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Constructor<?> controllerConstructor = c.getDeclaredConstructor();
+        Object controller = controllerConstructor.newInstance();
+
+        Parameter[] parameters = m.getParameters();
+        Object[] args = new Object[parameters.length];
+        for (int i = 0; i < parameters.length; i++) {
+            Parameter p = parameters[i];
+            String paramValue = req.getParameter(p.getName());
+            if (p.getType() == int.class) {
+                args[i] = paramValue != null ? Integer.parseInt(paramValue) : 0;
+            } else {
+                args[i] = paramValue;
+            }
+        }
+
+        return m.invoke(controller, args);
     }
 
     public Class<?> getC() {
