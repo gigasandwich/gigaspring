ba966b13b7596cfb3b80de8ee5e9aa5fa74ad2f7
Yvan Noah <yvannandy@gmail.com>
2025-12-17 10:59:52 +0300

    Fix literall syntax error xD

--- src/main/java/com/giga/spring/servlet/rest/ErrorResponse.java
+++ src/main/java/com/giga/spring/servlet/rest/ErrorResponse.java
@@ -2,6 +2,6 @@ package com.giga.spring.servlet.rest;
 
 public class ErrorResponse extends Response {
     public ErrorResponse(int code, Object data) {
-        super("success", code, data);
+        super("error", code, data);
     }
 }
